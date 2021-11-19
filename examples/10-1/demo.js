814a9d3ad91f8adb815be81f47608ec1f6662392
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2021-11-19 10:48:43 +0800

    Fall 2021 Update

---
+++ examples/10-1/demo.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+	if(false) {
+		//alert('true!');
+	}
+	else {
+		//alert('false!');
+	}
+
+	// Real conditions
+	if(1 === 2) {
+		//alert('true!');
+	}
+	/*else { // 'else' is optional
+		alert('false!');
+	}*/
+
+	// !== means 'not equivalent to'
+	if(1 !== 2) {
+		//alert('true!');
+	}
+
+	// Multiple separate conditions
+	if(2 === '2') {
+		//alert('Condition 1 is true!');
+	}
+	else if('a' === 'a') {
+		//alert('Condition 2 is true!');
+	}
+	else {
+		//alert('Neither is true QQ.');
+	}
+
+	// AND vs. OR in the same line
+	if(2 === 2 && 15 === 10) {	// && means AND
+		alert('Condition 1 is true.');
+	}
+	else if(2 !== 2 || 15 === 10) {	// || means OR
+		alert('Condition 2 is true.');
+	}
+	else {
+		alert('Neither is true.');
+	}
+};
