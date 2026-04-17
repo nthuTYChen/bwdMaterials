8bc520006fb5b541fe56b247f06116b44c323fbc
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2026-04-17 21:25:45 +0800

    Week 8 Updates

--- 2026/progress/8-2/8-2.js
+++ 2026/progress/8-2/8-2.js
@@ -1,23 +1,48 @@
+// An alert message would pop up immediately when the HTML file is opened in a browser
 // alert("Embedded!");
 
+// Get the current inner width of the browser window that loads the HTML file
+// Store that value in winWidth
 var winWidth = window.innerWidth;
+// Get the current inner height of the browser window that loads the HTML file
+// Store that value in winHeight
 var winHeight = window.innerHeight;
+// Print the window width to the console
 console.log(winWidth);
+// Print the window height to the console
 console.log(winHeight);
 
+// Change the current page's URL to "https://google.com"
+// window = the browser window
+// location = the current web address information
+// href = the full URL of the target page
+// Setting href to a new URL makes the browser go to that page
+// We need to mark off this statement, or you will always see Google in your browser
 // window.location.href = "https://google.com";
-// window.location.pathname = "/nosuchpath";
 
+// Run this function after the whole webpage has finished loading
 window.onload = function() {
 	// alert("Website is ready!");
 
+	// This line is commented out
+	// If used, it would print the whole HTML document object to the console
 	// console.log(document);
 
+	// Use the querySelector() function of an HTML document object to retrieve
+	// ONE HTML element based on a selector.
 	let em = document.querySelector("article > h1");
 	// console.log(em);
+	// Change the background color of that h1 element to pink
 	em.style.backgroundColor = "pink";
+	// Try to find h3 that is an immediate/direct child of an article, which
+	// does not exist in 8-2.html. 
 	let em2 = document.querySelector("article > h3");
+	// As a consequence, querySelector() returns "null", which means "nothing".
 	console.log(em2);
+	// Try to find sections that are an immediate child of article. There
+	// are multiple matches in 8-2.html, but querySelector() only returns the
+	// FIRST match.
 	let em3 = document.querySelector("article > section");
+	// So, change the first section inside article to have an orange background color.
 	em3.style.backgroundColor = "orange";
 };
--- 2026/progress/8-3/8-3.js
+++ 2026/progress/8-3/8-3.js
@@ -1,16 +1,39 @@
 // alert("Embedded!");
 
+// Make an array called num_array
+// An array is a list that can store multiple values in one variable
+// Here, the array stores five numbers: 100, 50, 3, 6, and 9
 var num_array = [100, 50, 3, 6, 9];
 
+// Print the whole array to the console
 console.log(num_array);
+// Print the value at index 0
+// Arrays use index numbers starting from 0, not 1
+// So index 0 is the first value: 100
 console.log(num_array[0]);
+// Print the value at index 1
+// Index 1 is the second value: 50
 console.log(num_array[1]);
+// Try to print the value at index 6
+// This array does not have a value at index 6
+// Its indexes are 0, 1, 2, 3, and 4
+// So this prints "undefined"
 console.log(num_array[6]);
-
+// Every array is also an OBJECT, and here we can try to retrieve its
+// "length" key. Length means the total number of items in the array
+// This array has 5 items
 console.log(num_array.length);
 
+// An array object comes with a forEach() function.
+// That is, "for each" value in the array, do something.
+// forEach goes through each value in the array one by one,
+// and pass each value one by one to another function defined
+// within forEach().
 num_array.forEach(
+	// When each value is passed to this function within forEach(),
+	// it is mapped to a variable "num"
 	function(num) {
+		// Print each value
 		console.log(num);
 	}
 );
